865b37789afb7be1b5628aeec2feb7f1f5deac8a
Levon Poghosyan <105727342+pelfegor@users.noreply.github.com>
2022-12-07 10:29:50 +0300

    Update Data.java

--- src/test/java/ru/netology/data/Data.java
+++ src/test/java/ru/netology/data/Data.java
@@ -2,6 +2,7 @@ package ru.netology.data;
 
 import lombok.NoArgsConstructor;
 import lombok.Value;
+import ru.netology.Generator;
 
 @NoArgsConstructor
 public class Data {
@@ -16,85 +17,89 @@ public class Data {
     }
 
     public static CardData getApprovedCard() {
-        return new CardData("4444 4444 4444 4441", "09", "24", "Ivanov Ivan", "123");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(1, 0), "Ivanov Ivan", "123");
     }
 
     public static CardData getDeclinedCard() {
-        return new CardData("4444 4444 4444 4442", "09", "24", "Ivanov Ivan", "123");
+        return new CardData("4444 4444 4444 4442", getMonth(0, 0), getYear(1, 0), "Ivanov Ivan", "123");
     }
 
  //
 
     public static CardData getInvalidCardNumberIfLess16Sym() {
-        return new CardData("4444 4444 4444 4", "09", "24", "Ivanov Ivan", "123");
+        return new CardData("4444 4444 4444 4", getMonth(0, 0), getYear(1, 0), "Ivanov Ivan", "123");
     }
 
     public static CardData getInvalidCardNumberIfOutOfBase() {
-        return new CardData("4445 4444 4444 4441", "09", "24", "Ivanov Ivan", "123");
+        return new CardData("4445 4444 4444 4441", getMonth(0, 0), getYear(1, 0), "Ivanov Ivan", "123");
     }
 
 //
 
     public static CardData getInvalidNumberOfMonthIfOneSym() {
-        return new CardData("4444 4444 4444 4441", "1", "24", "Ivanov Ivan", "123");
+        return new CardData("4444 4444 4444 4441", "1", getYear(1, 0), "Ivanov Ivan", "123");
     }
 
     public static CardData getInvalidNumberOfMonthIfMore12() {
-        return new CardData("4444 4444 4444 4441", "20", "24", "Ivanov Ivan", "123");
+        return new CardData("4444 4444 4444 4441", "20", getYear(1, 0), "Ivanov Ivan", "123");
     }
 
     public static CardData getInvalidNumberOfMonthIfZero() {
-        return new CardData("4444 4444 4444 4441", "00", "24", "Ivanov Ivan", "123");
+        return new CardData("4444 4444 4444 4441", "00", getYear(1, 0), "Ivanov Ivan", "123");
+    }
+    
+    public static CardData getInvalidNumberOfMonthIfLessThenThisMonthInCurrentYear() {
+        return new CardData("4444 4444 4444 4441", getMonth(0, 1), getYear(0, 0), "Ivanov Ivan", "123");
     }
 
  //
 
     public static CardData getInvalidYearIfBeforeCurrentYear() {
-        return new CardData("4444 4444 4444 4441", "09", "19", "Ivanov Ivan", "123");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(0, 1), "Ivanov Ivan", "123");
     }
 
     public static CardData getInvalidYearIfZero() {
-        return new CardData("4444 4444 4444 4441", "09", "00", "Ivanov Ivan", "123");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), "00", "Ivanov Ivan", "123");
     }
 
     public static CardData getInvalidYearIfInTheFarFuture() {
-        return new CardData("4444 4444 4444 4441", "09", "40", "Ivanov Ivan", "123");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(6, 0), "Ivanov Ivan", "123");
     }
 
 //
 
     public static CardData getInvalidCardholderNameIfSpace() {
-        return new CardData("4444 4444 4444 4441", "09", "24", " ", "123");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(1, 0), " ", "123");
     }
 
     public static CardData getInvalidCardholderNameIfOneWord() {
-        return new CardData("4444 4444 4444 4441", "09", "24", "Ivanov", "123");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(1, 0), "Ivanov", "123");
     }
 
     public static CardData getInvalidCardholderNameIfThreeWords() {
-        return new CardData("4444 4444 4444 4441", "09", "24", "Ivanov Ivan Petrovich", "123");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(1, 0), "Ivanov Ivan Petrovich", "123");
     }
 
     public static CardData getInvalidCardholderNameIfRusSym() {
-        return new CardData("4444 4444 4444 4441", "09", "24", "Иванов Иван", "123");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(1, 0), "Иванов Иван", "123");
     }
 
     public static CardData getInvalidCardholderNameIfNumeric() {
-        return new CardData("4444 4444 4444 4441", "09", "24", "5432 3232", "123");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(1, 0), "5432 3232", "123");
     }
 
     public static CardData getInvalidCardholderNameIfWildcard() {
-        return new CardData("4444 4444 4444 4441", "09", "24", "#%№", "123");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(1, 0), "#%№", "123");
     }
 
 //
 
     public static CardData getInvalidCvvIfTwoSym() {
-        return new CardData("4444 4444 4444 4441", "09", "24", "Ivanov Ivan", "12");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(1, 0), "Ivanov Ivan", "12");
     }
 
     public static CardData getInvalidCvvIfThreeZero() {
-        return new CardData("4444 4444 4444 4441", "09", "24", "Ivanov Ivan", "000");
+        return new CardData("4444 4444 4444 4441", getMonth(0, 0), getYear(1, 0), "Ivanov Ivan", "000");
     }
 
     public static CardData getInvalidCardDataIfEmptyAllFields() {
